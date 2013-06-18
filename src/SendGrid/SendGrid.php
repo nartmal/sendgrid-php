5952d9f0796957c0a00c3ccbd68b402402a3a440
Lam Tran <lam@recycledmedia.com>
2013-06-18 20:23:10 +0000

    strip out guts on SendGrid cuz autoloader in composer handels a lot of the stuff

--- src/SendGrid/SendGrid.php
+++ src/SendGrid/SendGrid.php
@@ -1,46 +1,34 @@
 <?php
+
 namespace SendGrid;
 
-class SendGrid
-{
-  const VERSION = "1.0.0";
+class SendGrid {
 
-  protected $namespace = "SendGrid",
+    const VERSION = "1.0.0";
+
+    protected $namespace = "SendGrid",
             $username,
             $password;
-
-  // Available transport mechanisms
-  protected $web,
+    // Available transport mechanisms
+            protected $web,
             $smtp;
-  
-  public function __construct($username, $password)
-  {
-	echo 3;
-    $this->username = $username;
-    $this->password = $password;
-  }
 
-  public function __get($api)
-  {
-    $name = $api;
-
-    if($this->$name != null)
-    {
-      return $this->$name;
+    public function __construct($username, $password) {
+        $this->username = $username;
+        $this->password = $password;
     }
 
-    $api = $this->namespace . "\\" . ucwords($api);
-    $class_name = str_replace('\\', '/', "$api.php");
-    $file = __dir__ . DIRECTORY_SEPARATOR . $class_name;
+    public function __get($api) {
+        $name = $api;
 
-    if (!file_exists($file))
-    {
-      throw new Exception("Api '$class_name' not found.");
+        if ($this->$name != null) {
+            return $this->$name;
+        }
+
+        $api = $this->namespace . "\\" . ucwords($api);
+        $class_name = str_replace('\\', '/', "$api.php");
+        $this->$name = new $api($this->username, $this->password);
+        return $this->$name;
     }
-    require_once $file;
-
-    $this->$name = new $api($this->username, $this->password);
-    return $this->$name;
-  }
 
 }
